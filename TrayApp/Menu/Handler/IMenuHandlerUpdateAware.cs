847c117f13b670447a86e1a88a637e17ea77f865
Erik Perri <erik.perri@gmail.com>
2020-04-09 05:03:27 -0400

    Cleanup

--- TrayApp/Menu/Handler/IMenuHandlerUpdateAware.cs
+++ TrayApp/Menu/Handler/IMenuHandlerUpdateAware.cs
@@ -1,6 +1,4 @@
-﻿using System.Windows.Forms;
-
-namespace TrayApp.Menu.Handler
+﻿namespace TrayApp.Menu.Handler
 {
     public interface IMenuHandlerUpdateAware
     {
--- TrayApp/TrayIconManager.cs
+++ TrayApp/TrayIconManager.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
 using System;
-using System.Diagnostics;
-using System.Drawing;
 using System.Windows.Forms;
 using TrayApp.Menu;
 
