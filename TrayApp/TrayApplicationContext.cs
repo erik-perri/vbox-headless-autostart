7c79480e9e8dcb40e7f9753a6be6b3b964f83af6
Erik Perri <erik.perri@gmail.com>
2020-04-10 12:04:24 -0400

    Added updating and recreation of context menu when machine state is changed

--- TrayApp/TrayApplicationContext.cs
+++ TrayApp/TrayApplicationContext.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Extensions.Logging;
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 using TrayApp.Menu;
 using TrayApp.VirtualMachine;
@@ -9,26 +11,75 @@ namespace TrayApp
     public class TrayApplicationContext : ApplicationContext
     {
         private readonly ILogger<TrayApplicationContext> logger;
-        private readonly TrayIconManager iconManager;
         private readonly TrayContextMenuStrip contextMenu;
-        private readonly MachineStoreUpdater machineStoreUpdater;
 
         public TrayApplicationContext(
             ILogger<TrayApplicationContext> logger,
-            TrayIconManager iconManager,
             TrayContextMenuStrip contextMenu,
-            MachineStoreUpdater machineUpdater
+            TrayIconManager iconManager,
+            MachineStore machineStore
         )
         {
             logger.LogTrace(".ctor");
 
+            if (iconManager == null)
+            {
+                throw new ArgumentNullException(nameof(iconManager));
+            }
+
+            if (machineStore == null)
+            {
+                throw new ArgumentNullException(nameof(machineStore));
+            }
+
             this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
             this.contextMenu = contextMenu ?? throw new ArgumentNullException(nameof(contextMenu));
-            this.iconManager = iconManager ?? throw new ArgumentNullException(nameof(iconManager));
-            this.machineStoreUpdater = machineUpdater ?? throw new ArgumentNullException(nameof(machineUpdater));
 
             iconManager.SetContextMenu(contextMenu);
             iconManager.Show();
+
+            machineStore.OnMachineChange += (object _, EventArgs __) => RecreateContextMenu();
+            machineStore.OnStateChange += (object _, EventArgs __) => UpdateContextMenu();
+
+            RecreateContextMenu();
+        }
+
+        private void UpdateContextMenu()
+        {
+            logger.LogTrace($"UpdateContextMenu");
+
+            if (contextMenu.InvokeRequired)
+            {
+                // TODO Figure out why this doesn't work without a new thread. If it is run in this thread the menu
+                //      fails to update and stops responding to the mouse with no errors.  If Invoke is removed we get
+                //      the normal cannot update control from a different thread error.
+                Task.Factory.StartNew(
+                    () => contextMenu.Invoke(new MethodInvoker(delegate { contextMenu.UpdateContextMenu(); })),
+                    CancellationToken.None,
+                    TaskCreationOptions.None,
+                    TaskScheduler.Default
+                );
+                return;
+            }
+
+            contextMenu.UpdateContextMenu();
+        }
+
+        private void RecreateContextMenu()
+        {
+            logger.LogTrace($"RecreateContextMenu");
+
+            if (contextMenu.InvokeRequired)
+            {
+                // TODO Same as UpdateContextMenu
+                Task.Factory.StartNew(
+                    () => contextMenu.Invoke(new MethodInvoker(delegate { contextMenu.CreateContextMenu(); })),
+                    CancellationToken.None,
+                    TaskCreationOptions.None,
+                    TaskScheduler.Default
+                );
+                return;
+            }
 
             contextMenu.CreateContextMenu();
         }
