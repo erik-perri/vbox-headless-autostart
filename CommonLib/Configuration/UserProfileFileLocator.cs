d0c7a7dbc726ab2c528cee37417608e34eebda48
Erik Perri <erik.perri@gmail.com>
2020-04-14 18:38:50 -0400

    Moved UserProfileLocator to CommonLib and added constructor option to override path

---
+++ CommonLib/Configuration/UserProfileFileLocator.cs
@@ -0,0 +1,31 @@
+﻿using CommonLib.Configuration;
+using System;
+using System.IO;
+
+namespace CommonLib.Configuration
+{
+    public class UserProfileFileLocator : IConfigurationFileLocator
+    {
+        private readonly string profilePath;
+
+        public UserProfileFileLocator()
+        {
+            profilePath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+        }
+
+        public UserProfileFileLocator(string profilePath)
+        {
+            if (!Directory.Exists(profilePath))
+            {
+                throw new ArgumentException($"User profile \"{profilePath}\" does not exist", nameof(profilePath));
+            }
+
+            this.profilePath = profilePath;
+        }
+
+        public string LocateFile()
+        {
+            return $"{profilePath}\\.VirtualBox\\VBoxHeadlessAutoStart.xml";
+        }
+    }
+}
