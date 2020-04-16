8bf63bba0eb96d82504bbbf206bf072ac57938e6
Erik Perri <erik.perri@gmail.com>
2020-04-16 16:07:57 -0400

    Added moved launch checkbox script to build events

---
+++ SetupProject/Helpers/MoveLaunchCheckbox.js
@@ -0,0 +1,14 @@
+// Moves the launch checkbox on the ExitDialog to the bottom right so it is over a gray background.
+// From https://stackoverflow.com/a/1801438
+var msiOpenDatabaseModeTransact = 1;
+var filespec = WScript.Arguments(0);
+var installer = new ActiveXObject("WindowsInstaller.Installer");
+var database = installer.OpenDatabase(filespec, msiOpenDatabaseModeTransact);
+var sql = "UPDATE `Control` SET `Control`.`Height` = '18', `Control`.`Width` = '170'," +
+          " `Control`.`Y`='243', `Control`.`X`='10' " +
+          "WHERE `Control`.`Dialog_`='ExitDialog' AND " + 
+          "  `Control`.`Control`='OptionalCheckBox'";
+var view = database.OpenView(sql);
+view.Execute();
+view.Close();
+database.Commit();
