e997b3d1b24bf4feeb19259d2c1ec0cc3843a679
Erik Perri <erik.perri@gmail.com>
2020-04-17 14:00:34 -0400

    Added application path configuration locator

---
+++ TrayApp/Configuration/FileLocator/ApplicationPathFileLocator.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace TrayApp.Configuration.FileLocator
+{
+    public class ApplicationPathFileLocator : IConfigurationFileLocator
+    {
+        private readonly string profilePath;
+
+        public ApplicationPathFileLocator()
+        {
+            var currentExecutable = Assembly.GetExecutingAssembly().Location;
+
+            profilePath = Path.GetDirectoryName(currentExecutable);
+        }
+
+        public string LocateFile()
+        {
+            return $"{profilePath}\\VBoxHeadlessAutoStart.xml";
+        }
+    }
+}
